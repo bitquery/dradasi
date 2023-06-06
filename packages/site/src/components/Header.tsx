95b46ab311d3e569ef90dc3378bea98360af91d2
Srdjan S <stankovic.srdjo@gmail.com>
2023-06-06 17:46:19 +0200

    add logo

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -3,7 +3,7 @@ import styled, { useTheme } from 'styled-components';
 import { MetamaskActions, MetaMaskContext } from '../hooks';
 import { connectSnap, getThemePreference, getSnap } from '../utils';
 import { HeaderButtons } from './Buttons';
-import { SnapLogo } from './SnapLogo';
+import { Logo } from './Logo';
 import { Toggle } from './Toggle';
 
 const HeaderWrapper = styled.header`
@@ -59,7 +59,7 @@ export const Header = () => {
   return (
     <HeaderWrapper>
       <LogoWrapper>
-        <SnapLogo color={theme.colors.icon.default} size={36} />
+        <Logo size={36} />
         <Title>DRADASI</Title>
       </LogoWrapper>
       <RightContainer>
--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -161,6 +161,53 @@ const Index = () => {
     }
   };
 
+  const handleIssueDID = async () => {
+    try {
+      const address = state.accounts[0] as string;
+
+      const issuer = new EthrDID({
+        identifier: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
+        privateKey:
+          'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75',
+      }) as Issuer;
+
+      const vcPayload: JwtCredentialPayload = {
+        sub: `did:ethr:${address}`,
+        nbf: 1562950282,
+        vc: {
+          '@context': ['https://www.w3.org/2018/credentials/v1'],
+          type: ['VerifiableCredential'],
+          credentialSubject: {
+            document: {
+              type: 'DriversLicence',
+              name: 'Dra',
+              surname: 'Dasi',
+            },
+          },
+        },
+      };
+
+      const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer);
+      const vpPayload: JwtPresentationPayload = {
+        vp: {
+          '@context': ['https://www.w3.org/2018/credentials/v1'],
+          type: ['VerifiablePresentation'],
+          verifiableCredential: [vcJwt],
+        },
+      };
+
+      const vpJwt = await createVerifiablePresentationJwt(vpPayload, issuer);
+      console.log(vcJwt);
+
+      const b = { [state.accounts[0]]: vpJwt };
+      const newDids = (await saveDid(b)) as Record<string, any>;
+      console.log('newSaveDids', newDids);
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
   /**
    * Verifiers jwt and handlers rent process. In non-hackaton world this would be moved
    * probably to some backend.
